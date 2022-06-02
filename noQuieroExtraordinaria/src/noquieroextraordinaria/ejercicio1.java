bae76374c434f9e1e88abdb4f750ee0e686e2864
DiosaEstelaa <estelagm0109@gmail.com>
2022-06-02 17:21:47 +0200

    funciona el git

--- noQuieroExtraordinaria/src/noquieroextraordinaria/ejercicio1.java
+++ noQuieroExtraordinaria/src/noquieroextraordinaria/ejercicio1.java
@@ -15,6 +15,8 @@ public class ejercicio1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("esto es una prueba");
     }
     
 }
